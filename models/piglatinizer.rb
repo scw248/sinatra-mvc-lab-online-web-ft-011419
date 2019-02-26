1c074e823159f70ad1352373868e477586294e0c
Stephen Wright <scw248@gmail.com>
2019-02-26 20:03:44 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -18,27 +18,39 @@ class PigLatinizer
       end
         phrase
    end
+   
+   def rearrange(word)
+    translation = word.split(' ').map do |w|
+    vowels = %w(a e i o u)
+    word = w.downcase.split('')
 
-   def rearrange(word) 
-    vowels = ["a", "e", "i", "o", "u", "y"]
-    new_word = word
-    
-    word.each_char do |char|
-      if char == "u" && new_word[-1] == "q"
-        new_word << "u"
-        new_word.slice!(0)
-      next
-      elsif vowels.include? char
-        new_word << "ay"
-        break
-      else
-        new_word << char
-        new_word.slice!(0)
-      next
-      end
+    if vowels.include? word[0]
+      "#{word.join}way"
+    else
+      "%s%say" % [word[1..-1].join, word[0]]
+    end
   end
-  new_word
-end
+
+#   def rearrange(word) 
+#     vowels = ["a", "e", "i", "o", "u", "y"]
+#     new_word = word
+    
+#     word.each_char do |char|
+#       if char == "u" && new_word[-1] == "q"
+#         new_word << "u"
+#         new_word.slice!(0)
+#       next
+#       elsif vowels.include? char
+#         new_word << "ay"
+#         break
+#       else
+#         new_word << char
+#         new_word.slice!(0)
+#       next
+#       end
+#   end
+#   new_word
+# end
 
    
 end
