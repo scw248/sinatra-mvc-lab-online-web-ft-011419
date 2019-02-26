bb9b235362cbd1e109834551fcfc65f07d2adc05
Stephen Wright <scw248@gmail.com>
2019-02-26 21:17:25 +0000

    Automatically backed up by Learn

--- spec/sinatra_mvc_lab_spec.rb
+++ spec/sinatra_mvc_lab_spec.rb
@@ -57,6 +57,8 @@ describe "Pig Latinizer App" do
 
     it "displays the pig latinized phrase upon form submission" do
       expect(last_response.body).to include("eHay asway anway oldway anmay owhay ishedfay aloneway inway away iffskay inway ethay ulfGay eamStray andway ehay adhay onegay eightyway ourfay aysday ownay ithoutway akingtay away ishfay")
+      
+       eHay asway anway olday anmay owhay ishedfay aloneay inway away iffskay inway ethay ulfGay eamStray andway ehay adhay onegay eightyay ourfay aysdayownay ithoutway akingtay away ishfay
     end
   end
 
