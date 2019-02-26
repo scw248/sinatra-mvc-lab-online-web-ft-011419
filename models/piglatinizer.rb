ed7f20a588aa101e07154f2b6d65638ac104bc18
Stephen Wright <scw248@gmail.com>
2019-02-26 20:07:53 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,51 +6,27 @@ class PigLatinizer
     @string = string
    end
    
-   def piglatinize(string)
-      phrase = []
-      if string.include? " "
-        phrase = string.split(" ").map do |word|
-        rearrange(word)
-        end
-          phrase = phrase.join(" ")
-      else
-        phrase = rearrange(string)
-      end
-        phrase
-   end
-   
-   def rearrange(word)
-    translation = word.split(' ').map do |w|
-    vowels = %w(a e i o u)
-    word = w.downcase.split('')
-
-    if vowels.include? word[0]
-      "#{word.join}way"
-    else
-      "%s%say" % [word[1..-1].join, word[0]]
-    end
-  end
-
-#   def rearrange(word) 
-#     vowels = ["a", "e", "i", "o", "u", "y"]
-#     new_word = word
     
-#     word.each_char do |char|
-#       if char == "u" && new_word[-1] == "q"
-#         new_word << "u"
-#         new_word.slice!(0)
-#       next
-#       elsif vowels.include? char
-#         new_word << "ay"
-#         break
-#       else
-#         new_word << char
-#         new_word.slice!(0)
-#       next
-#       end
-#   end
-#   new_word
-# end
+def piglatinize(word)
+  non_pig_latin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
+  vowels = ["a", "e", "i", "o", "u"]
 
-   
+  if non_pig_latin_words.include?(word)
+    word
+  elsif vowels.include? word[0]
+    word << "ay"
+  else
+    consonants = ""
+    while !vowels.include?(word[0])
+      consonants << word[0]
+      word = word.split("")[1..-1].join
+    end
+    word + consonants + 'ay'
+  end
+end
+
+
+def to_pig_latin(string)
+  string.split.collect{|word| piglatinize(word)}.join(" ")
+end
 end
