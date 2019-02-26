27b84097758230c654c050bcdb8a9dd4926d287d
Stephen Wright <scw248@gmail.com>
2019-02-26 20:49:48 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -38,7 +38,7 @@ class PigLatinizer
 end
 
 
-  def to_pig_latin(string)
-    string.split.collect{|word| piglatinize(word)}.join(" ")
-  end
-end
+#   def to_pig_latin(string)
+#     string.split.collect{|word| piglatinize(word)}.join(" ")
+#   end
+# end
