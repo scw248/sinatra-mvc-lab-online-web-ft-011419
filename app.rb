3c221018cb88b70c2d6b39351bf376ff4be5e470
Stephen Wright <scw248@gmail.com>
2019-02-26 19:45:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get "/" do
+    erb :user_input
+  end
+  
+  post "/" do
+    string = params[:string]
+    @pig_latin_string = PigLatinizer.new(string)
+    erb :results
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,24 @@
+class PigLatinizer
+  
+  attr_reader :string
+  
+   def initialize(string: nil)
+    @string = string
+   end
+   
+   def piglatinize(string)
+     alpha = ('a'..'z').to_a
+     vowels = %w[a e i o u]
+     consonants = alpha - vowels
+
+      if vowels.include?(string[0])
+        string + 'way'
+      elsif consonants.include?(string[0]) && consonants.include?(string[1])
+        string[2..-1] + string[0..1] + 'ay'
+      elsif consonants.include?(string[0])
+        string[1..-1] + string[0] + 'ay'
+      else
+        string + 'way'
+      end
+   end
+end
