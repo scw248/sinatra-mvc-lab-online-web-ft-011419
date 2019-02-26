c8188d16fcca7fce53420cdb5dbd26bf89c36fdb
Stephen Wright <scw248@gmail.com>
2019-02-26 20:08:35 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,16 +7,17 @@ class PigLatinizer
    end
    
     
-def piglatinize(word)
-  non_pig_latin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
-  vowels = ["a", "e", "i", "o", "u"]
+  def piglatinize(word)
+    non_pig_latin_words = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
+    vowels = ["a", "e", "i", "o", "u"]
 
-  if non_pig_latin_words.include?(word)
-    word
-  elsif vowels.include? word[0]
-    word << "ay"
-  else
-    consonants = ""
+    if non_pig_latin_words.include?(word)
+      word
+    elsif vowels.include? word[0]
+      word << "ay"
+    else
+      consonants = ""
+      
     while !vowels.include?(word[0])
       consonants << word[0]
       word = word.split("")[1..-1].join
@@ -26,7 +27,7 @@ def piglatinize(word)
 end
 
 
-def to_pig_latin(string)
-  string.split.collect{|word| piglatinize(word)}.join(" ")
-end
+  def to_pig_latin(string)
+    string.split.collect{|word| piglatinize(word)}.join(" ")
+  end
 end
