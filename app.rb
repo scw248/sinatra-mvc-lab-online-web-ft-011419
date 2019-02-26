0ae1f24c8936291452602837e0d7dd539bbedccb
Stephen Wright <scw248@gmail.com>
2019-02-26 21:02:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,6 @@ class App < Sinatra::Base
   post "/piglatinize" do
     user_phrase = params[:user_phrase]
     @pig_latin_string = PigLatinizer.new(user_phrase)
-    @pig_latin_string
     erb :results
   end
 end
